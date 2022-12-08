c4f81d2d71fc3684939a4486fa1d76040b8b863d
sjmedler <71857162+sjmedler@users.noreply.github.com>
2022-12-08 16:48:24 -0500

    Update authorization.py

--- authorization.py
+++ authorization.py
@@ -1,7 +1,7 @@
 import tekore as tk
 
 def authorize():
-    CLIENT_ID = "8b6f1243968d4e4cada4c1f45763409a"
-    CLIENT_SECRET = "afbe6d6090ea47d3829fc015e7fadc1b"
+    CLIENT_ID = "hidden"
+    CLIENT_SECRET = "hidden"
     app_token = tk.request_client_token(CLIENT_ID, CLIENT_SECRET)
     return tk.Spotify(app_token)
