5ab9c1150810a4e6324fdd8c417aa7091b822e85
sjmedler <71857162+sjmedler@users.noreply.github.com>
2022-12-11 04:19:30 -0500

    Delete authorization.py

--- authorization.py
+++
@@ -1,7 +0,0 @@
-import tekore as tk
-
-def authorize():
-    CLIENT_ID = "hidden"
-    CLIENT_SECRET = "hidden"
-    app_token = tk.request_client_token(CLIENT_ID, CLIENT_SECRET)
-    return tk.Spotify(app_token)
